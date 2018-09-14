b54307afae47d5d689ca922e34b3e5d43987736b
Chet Corcos <ccorcos@gmail.com>
2018-09-14 14:41:40 -0700

    Test busting the cache.

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ const app = express()
 
 const version = "v1"
 
+app.use(express.static(__dirname + "/" + version))
+
 app.get("*", function(req, res) {
-	if (req.path === "/index.appcache") {
-		res.sendFile(version + "/index.appcache", { root: __dirname })
-	} else if (req.path === "/index.js") {
-		res.sendFile(version + "/index.js", { root: __dirname })
-	} else if (req.path === "/favicon.ico") {
+	if (req.path === "/favicon.ico") {
 		res.status(404).send("")
 	} else {
 		res.sendFile(version + "/index.html", { root: __dirname })
 	}
 })
 
-app.listen(5001, () => console.log("Listening on port 5000"))
+app.listen(8003, () => console.log("Listening on port 8003"))
--- v1/index.js
+++ v1/index.js
@@ -1,6 +1,33 @@
 function updateAppCache() {
-	console.log("updating application cache")
+	console.log("updating application cache.")
 	window.applicationCache.update()
 }
+
+window.applicationCache.addEventListener("error", error => {
+	console.log("application cache error.")
+	console.log(error)
+})
+
+window.applicationCache.addEventListener("updateready", () => {
+	console.log("updateready application cache.")
+	console.log("swapping application cache.")
+	window.applicationCache.swapCache()
+})
+
+window.applicationCache.addEventListener("checking", () => {
+	console.log("checking application cache.")
+})
+
+window.applicationCache.addEventListener("downloading", () => {
+	console.log("downloading application cache.")
+})
+
+window.applicationCache.addEventListener("noupdate", () => {
+	console.log("noupdate application cache.")
+})
+
+window.applicationCache.addEventListener("obsolete", () => {
+	console.log("obsolete application cache.")
+})
 
 console.log("v1 js")
